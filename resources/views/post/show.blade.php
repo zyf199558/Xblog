db6bbaf14234d335626f9e58ec5f11b724999854
Lufficc Li <lufficc.lisap.com>
2017-09-06 11:16:14 +0800

    change to col-md-10

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2 col-sm-12 phone-no-padding">
+            <div class="col-md-10 col-md-offset-1 col-sm-12 phone-no-padding">
                 <div class="post-detail">
                     <div class="center-block">
                         <div class="post-detail-title">{{ $post->title }}</div>
