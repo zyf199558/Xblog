88613af762e51c21199df18fd5e156bc858e7a66
lufficc <luffy.lcc@gmail.com>
2017-09-07 15:04:14 +0800

    update search sytle

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -6,17 +6,17 @@
             <div class="col-md-8">
                 @if($posts->count() == 0)
                     <div class="card">
-                        <div class="card-header">
-                            搜索 "{{ request('q') }}"
-                        </div>
                         <div class="card-body">
-                            <h4>什么也没搜到...</h4>
+                            <h4 class="card-title">搜索 "{{ request('q') }}"</h4>
+                            <p class="card-text">什么也没搜到...</p>
                         </div>
                     </div>
                 @else
                     <div class="card mb-3">
-                        <div class="card-header">
-                            Search for "{{ request('q') }}"
+                        <div class="card-body">
+                            <h4 class="card-title">
+                                Search for "{{ request('q') }}"
+                            </h4>
                         </div>
                     </div>
                     @each('post.item',$posts,'post')
